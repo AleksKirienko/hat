3cdbcfe8dc609dc15e5270a3c32c37d8700d1b31
AleksKirienko <aleks.Kirienko@bk.ru>
2020-08-24 13:56:16 +0700

    Update api

--- src/app/core/game/game.service.ts
+++ src/app/core/game/game.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { GameItem } from '../models/game-item';
 import { map } from 'rxjs/operators';
 import { GameApiResponse } from '../models/GameApiResponse';
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class GameService {
@@ -12,7 +13,7 @@ export class GameService {
   }
 
   getData(): Observable<GameItem[]> {
-    return this.http.get<GameApiResponse>('/game').pipe(
+    return this.http.get<GameApiResponse>(`/api/game`).pipe(
       map((words: GameApiResponse) => words.articles)
     );
   }
